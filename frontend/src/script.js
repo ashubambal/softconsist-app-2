d3c3a11cd70ee065ccd63921955e22263bf1c5c8
ashubambal <ashutoshbambal97@gmail.com>
2025-12-26 16:45:25 +0530

    u

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -7,18 +7,13 @@ function loadProducts() {
       const list = document.getElementById("list");
       list.innerHTML = "";
 
-      if (data.length === 0) {
-        list.innerHTML = "<p class='empty'>No products available</p>";
-        return;
-      }
-
       data.forEach(p => {
         list.innerHTML += `
           <div class="product-card">
             <h3>${p.name}</h3>
             <p class="price">₹${p.price}</p>
-            <button class="btn-danger" onclick="deleteProduct(${p.id})">
-              Delete
+            <button onclick="addToCart('${p.name}', ${p.price})">
+              Add to Cart
             </button>
           </div>
         `;
@@ -26,31 +21,9 @@ function loadProducts() {
     });
 }
 
-function addProduct() {
-  const name = document.getElementById("name").value;
-  const price = document.getElementById("price").value;
-
-  if (!name || !price) {
-    alert("Please enter product name and price");
-    return;
-  }
-
-  fetch(API, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name, price })
-  }).then(() => {
-    document.getElementById("name").value = "";
-    document.getElementById("price").value = "";
-    loadProducts();
-  });
-}
-
-function deleteProduct(id) {
-  fetch(`${API}/${id}`, { method: "DELETE" })
-    .then(loadProducts);
+function addToCart(name, price) {
+  alert(`${name} added to cart 🛒`);
 }
 
 loadProducts();
-
 
